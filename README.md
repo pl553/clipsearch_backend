67616fbc088bf9dafd1eecfcae022a6b272ee569
pl553 <110288373+pl553@users.noreply.github.com>
2023-07-31 00:57:16 +0300

    image adding api route

--- README.md
+++ README.md
@@ -1,15 +1,32 @@
+# Building
+```bash
+go mod tidy
+go build
+```
 # Database
 ## Initial setup
-`sudo -i -u postgres`  
-`psql`  
-`postgres=# create user clipsearch with password 'weakpassword'`    
-`\q`   
-`createdb clipsearch`  
-`exit`  
-`export POSTGRESQL_URL=postgres://clipsearch:weakpassword@localhost:5432/clipsearch?sslmode=disable`  
+```bash
+sudo -i -u postgres  
+psql  
+```
+
+```
+postgres=# create user clipsearch with password 'weakpassword'
+\q
+```
+
+```bash
+createdb clipsearch  
+exit
+export POSTGRESQL_URL=postgres://clipsearch:weakpassword@localhost:5432/clipsearch?sslmode=disable
+```
 ## Creating/running migrations on the cli
 https://github.com/golang-migrate/migrate/blob/master/database/postgres/TUTORIAL.md
 ### Creating a new migration
-`migrate create -ext sql -dir db/migrations -seq create_users_table`
+```bash
+migrate create -ext sql -dir db/migrations -seq create_users_table
+```
 ### Running migrations
-`migrate -database ${POSTGRESQL_URL} -path db/migrations up` 
+```bash
+migrate -database ${POSTGRESQL_URL} -path db/migrations up
+```
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 
 	"clipsearch/config"
@@ -16,13 +17,42 @@ import (
 
 var pgPool *pgxpool.Pool
 
-func getGallery(c *gin.Context) {
+func postGalleryImages(c *gin.Context) {
+	c.Request.ParseForm()
+	rawUrl := c.Request.Form.Get("url")
+	if rawUrl == "" {
+		c.JSON(http.StatusBadRequest, jsend.NewFail(gin.H{"url": "Image url must be non-empty"}))
+		return
+	}
+	invalidUrlMessage := jsend.NewFail(gin.H{"url": "Invalid url"})
+	url, err := url.ParseRequestURI(rawUrl)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, invalidUrlMessage)
+		return
+	}
+	if len(url.Query()) != 0 {
+		c.JSON(http.StatusBadRequest, jsend.NewFail(gin.H{"url": "Url must not have any query parameters in it"}))
+		return
+	}
+	// TODO add more validation (check that its actually an image, check that the filesize isnt too large, ...)
+	query := `INSERT INTO images (url) VALUES ($1);`
+	_, err = pgPool.Query(context.Background(), query, rawUrl)
+	if err != nil {
+		log.Printf("POST /api/gallery/images: failed to execute query %v", query)
+		log.Print(err)
+		c.JSON(http.StatusInternalServerError, jsend.NewError("Internal error", 500, nil))
+		return
+	}
+	c.JSON(http.StatusOK, jsend.New(nil))
+}
+
+func getGalleryImages(c *gin.Context) {
 	imageUrls := make([]string, 0, 100)
 	rows, err := pgPool.Query(context.Background(), `SELECT url FROM images;`)
 	if err != nil {
 		log.Print("Failed to get image urls from db")
 		log.Print(err)
-		c.JSON(http.StatusInternalServerError, jsend.NewError("Failed to get images", 199, nil))
+		c.JSON(http.StatusInternalServerError, jsend.NewError("Failed to get images", 500, nil))
 		return
 	}
 	for rows.Next() {
@@ -67,6 +97,7 @@ func main() {
 	}
 	router := gin.New()
 	router.Use(gin.Recovery())
-	router.GET("/api/gallery", getGallery)
+	router.GET("/api/gallery/images", getGalleryImages)
+	router.POST("/api/gallery/images", postGalleryImages)
 	router.Run("localhost:" + port)
 }
